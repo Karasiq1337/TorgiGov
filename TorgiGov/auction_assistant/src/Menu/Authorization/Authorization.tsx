a04974996ed32acb57d6afc16f0a65feb2efceef
Karasiq1337 <johnnovikov@gmail.com>
2024-06-01 13:37:41 +0500

    Аворизацонное окно

---
+++ TorgiGov/auction_assistant/src/Menu/Authorization/Authorization.tsx
@@ -0,0 +1,54 @@
+﻿import React, {Children} from "react";
+import './Authorization.css'
+
+export function Authorization(){
+    return(
+        <VerticalAllinWrapper>
+           <AuthorizationElement/>
+        </VerticalAllinWrapper>
+    )
+}
+
+export function VerticalAllinWrapper({children}: { children: React.ReactNode }){
+    return(
+        <div className="outer">
+            <div className="middle">
+                <div className="inner">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function AuthorizationElement(){
+    return(
+        <div className="grid">
+            <div>
+                <text>
+                    Авторизоваться
+                </text>
+            </div>
+            <div>
+                <span>
+                    <input/>
+                </span>
+            </div>
+            <div>
+                <span>
+                    <input/>
+                </span>
+            </div>
+            <div>
+                <button>
+                    Войти
+                </button>
+            </div>
+            <div>
+                <span>
+                    Уже зарегестрированы?
+                </span>
+            </div>
+        </div>
+    )
+}
--- TorgiGov/auction_assistant/src/index.tsx
+++ TorgiGov/auction_assistant/src/index.tsx
@@ -2,12 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import {Authorization} from "./Menu/Authorization/Authorization";
+
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+    {
+        path: "login",
+        element: <Authorization/>
+    }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
+
