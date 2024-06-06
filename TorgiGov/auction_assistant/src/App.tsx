f0f3e3bb8a1cd8569c16998d56697f3d3ccc0398
Karasiq1337 <johnnovikov@gmail.com>
2024-06-06 22:20:20 +0500

    Удаляем чмще

--- TorgiGov/auction_assistant/src/App.tsx
+++ TorgiGov/auction_assistant/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {NavBar} from "./Menu/NavBar";
-import {Route, Router, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import {Home} from "./Menu/HomePage/Home";
 
 function App() {
--- TorgiGov/auction_assistant/src/index.tsx
+++ TorgiGov/auction_assistant/src/index.tsx
@@ -2,37 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {
-    BrowserRouter,
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-import {Authorization} from "./Menu/Authorization/Authorization";
-import "bootstrap/dist/css/bootstrap-grid.min.css"
-import {NavBar} from "./Menu/NavBar";
-import {Home} from "./Menu/HomePage/Home";
+import { BrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <div></div>,
-    },
-    {
-        path: "login",
-        element: <Authorization/>
-    },
-    {
-        path: "Home",
-        element: <Home/>
-    }
-]);
+import "bootstrap/dist/css/bootstrap-grid.min.css"
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
     <BrowserRouter>
         <App/>  
     </BrowserRouter>
