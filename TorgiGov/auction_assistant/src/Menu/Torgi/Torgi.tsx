61f5957ec91c77a4a24f6e7ecf16c02d2682ed85
Karasiq1337 <johnnovikov@gmail.com>
2024-06-12 17:47:39 +0500

    ПАДМУЗЫКУ

---
+++ TorgiGov/auction_assistant/src/Menu/Torgi/Torgi.tsx
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import {Container} from "react-bootstrap";
+
+export function Torgi() {
+    return (
+        <>
+            <Container>
+                
+            </Container>
+        </>
+    )
+}
