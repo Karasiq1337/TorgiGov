c9f8a68dfdf6846a5c5cea048406cf74fc4bfc1f
Timofey <demidovtimofey1@gmail.com>
2024-06-17 14:12:50 +0500

    home

--- TorgiGov/auction_assistant/src/Menu/HomePage/Home.tsx
+++ TorgiGov/auction_assistant/src/Menu/HomePage/Home.tsx
@@ -1,4 +1,4 @@
-import {Carousel, Container, Image, Row} from "react-bootstrap";
+import {Carousel, Col, Container, Image, ListGroup, Row} from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import React from "react";
 import Auctionlogo from 'Pictures/AuctionAssistantlogo.png';
@@ -8,35 +8,66 @@ export function Home(){
         <Container>
             <Form>
                 <Row>
-                    <h1 className={'text-center mt-5'}>Добро пожаловать</h1>
-                    <p></p>
+                    <blockquote className="blockquote">
+                        <h1 className={'text-center mt-5'}>Добро пожаловать </h1>
+                        <footer className="blockquote-footer text-center mt-2">Ваш помощник в электронных аукционах на земельные участки
+                        </footer>
+                    </blockquote>
                 </Row>
             </Form>
-    <Carousel>
+            <Row>
+                <Col>
+            <Carousel className={'w-100 h-100 mb-5'}>
         <Carousel.Item>
-            <img className={'d-block w-100'} src={'Pictures/AuctionAssistantlogo.png'} alt={'Чумба'}/>
-            <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <img className={'d-block w-100 h-50'} src={'Pictures/AuctionGrad4.png'} alt={'Чумба'}/>
+            <Carousel.Caption className={'text-primary-emphasis w-70 h-50'}>
+                <h3></h3>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-            <img className={'d-block w-100'} src={'Pictures/AuctionAssistantlogo.png'} alt={'Чумба'}/>
-            <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <img className={'d-block w-100 h-50'} src={'Pictures/AuctionGrad2.png'} alt={'Чумба'}/>
+            <Carousel.Caption className={'text-primary'}>
+                <h3></h3>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-            <img className={'d-block w-100'} src={'Pictures/AuctionAssistantlogo.png'} alt={'Чумба'}/>
-            <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                </p>
+            <img className={'d-block w-100 h-50'} src={'Pictures/AuctionGrad3.png'} alt={'Чумба'}/>
+            <Carousel.Caption className={'text-primary'}>
+                <h3></h3>
             </Carousel.Caption>
         </Carousel.Item>
     </Carousel>
+                </Col>
+                <Col>
+                    <Form className={'border border-primary-subtle border-4 mb-5'}>
+                    <h2 className={'bg-info text-center text-body-secondary'}>Что мы предлагаем:</h2>
+                        <h3 className={'ms-4'}><em>Поиск лотов</em></h3>
+                    <ListGroup as="ol">
+                        <ListGroup.Item as="li" variant={'primary'}>Удобство в использывании</ListGroup.Item>
+                        <ListGroup.Item as="li" variant={'primary'}>Только актуальные торги</ListGroup.Item>
+                        <ListGroup.Item as="li" variant={'primary'}>Сохраняйте понравившиеся лоты</ListGroup.Item>
+                    </ListGroup>
+                    <h3 className={'ms-4 mt-2'}><em>Сравнение лотов</em></h3>
+                    <ListGroup as="ol">
+                        <ListGroup.Item as="li" variant={'primary'}>Удобство в использывании</ListGroup.Item>
+                        <ListGroup.Item as="li" variant={'primary'}>Только актуальные торги</ListGroup.Item>
+                        <ListGroup.Item as="li" variant={'primary'}>Сохраняйте понравившиеся лоты</ListGroup.Item>
+                    </ListGroup>
+                    <h3 className={'ms-4 mt-2'}><em>Избранное</em></h3>
+                    <ListGroup as="ol">
+                        <ListGroup.Item as="li" variant={'primary'}>Удобство в использывании</ListGroup.Item>
+                        <ListGroup.Item as="li" variant={'primary'}>Только актуальные торги</ListGroup.Item>
+                        <ListGroup.Item as="li" variant={'primary'}>Сохраняйте понравившиеся лоты</ListGroup.Item>
+                    </ListGroup>
+                        <h3 className={'ms-4 mt-2'}><em>Статистика</em></h3>
+                    <ListGroup as="li">
+                        <ListGroup.Item as="li" variant={'primary'}>Удобство в использывании</ListGroup.Item>
+                        <ListGroup.Item as="li" variant={'primary'}>Только актуальные торги</ListGroup.Item>
+                        <ListGroup.Item as="li" variant={'primary'}>Сохраняйте понравившиеся лоты</ListGroup.Item>
+                    </ListGroup>
+                    </Form>
+                </Col>
+            </Row>
         </Container>
     )
 }
